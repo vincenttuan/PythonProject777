f03381b052efe77a5f91054562a521505ab8b673
vincentjava <vincentjava@yahoo.com.tw>
2020-12-30 19:27:15 +0800

    Raspberry Pi

--- gpio_zero/button/Button_LED_Buzeer.py
+++ gpio_zero/button/Button_LED_Buzeer.py
@@ -1,16 +1,20 @@
-from gpiozero import Button, LED
+from gpiozero import Button, LED, Buzzer
 from signal import pause
+from time import sleep
 
 led = LED(21)
 button = Button(20, pull_up=False)
+buzeer = Buzzer(16)
 
 def on():
     led.on()
     print(3.3)
+    buzeer.on()
 
 def off():
     led.off()
     print(0)
+    buzeer.off()
 
 button.when_pressed = on
 button.when_released = off
