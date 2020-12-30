6c40d30f84e4df24ff01d504693fa8fc8dab3459
vincentjava <vincentjava@yahoo.com.tw>
2020-12-30 19:54:42 +0800

    Raspberry Pi

--- gpio_rpi/buzeer/DoReMi.py
+++ gpio_rpi/buzeer/DoReMi.py
@@ -3,9 +3,9 @@ import RPi.GPIO as GPIO
 
 def doReMi():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT) # GPIO.PWM(channel, frequency)
     p = GPIO.PWM(16, 50)
-    p.start(15)
+    p.start(15) # 代表占空比（范围：0.0 <= dc >= 100.0）
 
     print("Do")
     p.ChangeFrequency(523)
