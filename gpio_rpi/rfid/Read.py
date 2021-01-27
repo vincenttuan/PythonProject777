4ce60e88d4bf9eb78cb9a8d34b2cc8b693d8c7fd
vincentjava <vincentjava@yahoo.com.tw>
2021-01-27 19:45:57 +0800

    Raspberry Pi RFID

---
+++ gpio_rpi/rfid/Read.py
@@ -0,0 +1,16 @@
+# https://pypi.org/project/mfrc522/
+from time import sleep
+import sys
+from RPi import GPIO
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id, text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
